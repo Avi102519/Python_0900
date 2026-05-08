07ce28ea9e5d2ea6145a91b5a485ace448f57b99
Avi102519 <aviswe2016@gmail.com>
2026-05-07 21:50:05 -0500

    Add files via upload

---
+++ Python_Operator.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Operators
+
+# # Arithmetic Operators
+# ( + , -, *, /, %, %%, **, ^)
+
+# In[2]:
+
+
+x,y = 4,8
+
+
+# In[3]:
+
+
+x+y
+
+
+# In[4]:
+
+
+x-y
+
+
+# In[5]:
+
+
+x/y
+
+
+# In[6]:
+
+
+x//y
+
+
+# In[7]:
+
+
+x%y
+
+
+# In[8]:
+
+
+y%x
+
+
+# In[9]:
+
+
+x**y
+
+
+# In[11]:
+
+
+x1=2
+y1=3
+x1**y1
+
+
+# # Assignment Operator (=)
+
+# In[18]:
+
+
+x2=1
+
+
+# In[19]:
+
+
+x2=x2+2
+
+
+# In[20]:
+
+
+x2
+
+
+# In[16]:
+
+
+y2 =3
+
+
+# In[17]:
+
+
+y2 = y2+2
+y2
+
+
+# In[21]:
+
+
+x2+=2
+x2
+
+
+# In[22]:
+
+
+x2 +=2
+x2
+
+
+# In[23]:
+
+
+x*=2
+x
+
+
+# In[24]:
+
+
+x-=2
+
+
+# In[25]:
+
+
+x
+
+
+# In[26]:
+
+
+x/=2
+
+
+# In[27]:
+
+
+x
+
+
+# In[28]:
+
+
+x//=2
+x
+
+
+# In[29]:
+
+
+a,b=5,6 # Values can be assigned in one line as well
+
+
+# In[30]:
+
+
+a
+
+
+# In[31]:
+
+
+b
+
+
+# # Unary Operator
+# Unary means 1|| Binary means 2
+# Here we are applyiong unary minus operator(-) on the operand n;the value of m becomes -n 
+# which indicates negative value
+
+# In[33]:
+
+
+n= 7
+m= -(n)
+m
+
+
+# # Relational operator
+# we are using this operator for comparing
+
+# In[34]:
+
+
+a=5
+b=6
+
+
+# In[35]:
+
+
+a<b
+
+
+# In[36]:
+
+
+a>b
+
+
+# In[37]:
+
+
+#for comparision we can use = symbol, This is assignment operator
+#For comparision use ==
+
+
+# In[38]:
+
+
+a==b
+
+
+# In[39]:
+
+
+a!=b
+
+
+# In[40]:
+
+
+a<=b
+
+
+# In[41]:
+
+
+a=<b
+
+
+# In[42]:
+
+
+a>=b
+
+
+# In[43]:
+
+
+a=>b
+
+
+# In[44]:
+
+
+a<!b
+
+
+# In[45]:
+
+
+a!<b
+
+
+# # Logical Operator
+# 3 logical operators--> And(*),or(+),Not(~)
+
+# In[48]:
+
+
+a=5
+b=2
+
+
+# In[51]:
+
+
+a<8 and b!=2
+
+
+# In[52]:
+
+
+a<8 and b>2
+
+
+# In[53]:
+
+
+a<8 or b<2
+
+
+# In[54]:
+
+
+a>8 or b<2
+
+
+# In[55]:
+
+
+x= False
+x
+
+
+# In[56]:
+
+
+not x
+
+
+# In[ ]:
+
+
+
+
