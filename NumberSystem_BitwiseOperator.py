d9dc30ee78911764489128c7a43beaa657f1c76c
Avi102519 <aviswe2016@gmail.com>
2026-05-09 08:37:01 -0500

    Add files via upload
    Python number system and bit wise operator

---
+++ NumberSystem_BitwiseOperator.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Binary Number System
+
+# In[1]:
+
+
+bin(25)
+
+
+# In[10]:
+
+
+int(0b11001)
+
+
+# In[11]:
+
+
+oct(25)
+
+
+# In[12]:
+
+
+int(0o31)
+
+
+# In[13]:
+
+
+hex(25)
+
+
+# In[14]:
+
+
+int(0x19)
+
+
+# In[15]:
+
+
+0xf
+
+
+# # 6Types of Bitwise
+# 
+# - complement(~)
+# - and(*)
+# - or(+)
+# - xor (xclusive or) (x'y+y'x)
+# - left shift <<
+# - right shift >>
+
+# In[16]:
+
+
+~12
+
+
+# In[17]:
+
+
+~3
+
+
+# # Bitwise Operator
+
+# In[18]:
+
+
+12&13
+
+
+# In[19]:
+
+
+12|13
+
+
+# In[20]:
+
+
+1&0
+
+
+# In[21]:
+
+
+1|0
+
+
+# In[2]:
+
+
+10&12
+
+
+# In[3]:
+
+
+12^13
+
+
+# In[4]:
+
+
+25^13
+
+
+# In[5]:
+
+
+12<<2
+
+
+# In[6]:
+
+
+13<<1
+
+
+# In[7]:
+
+
+12>>2
+
+
+# In[8]:
+
+
+13>>1
+
+
+# In[22]:
+
+
+print(bin(40))
+
+
+# In[23]:
+
+
+print(bin(34))
+
+
+# In[24]:
+
+
+bin(10)
+
+
+# In[25]:
+
+
+name1 = 'fine'
+
+
+# In[26]:
+
+
+name1[0:1] = 'd'
+
+
+# In[27]:
+
+
+name1[1:]
+
+
+# In[28]:
+
+
+'d'+name1[1:]
+
+
+# In[ ]:
+
+
+
+
