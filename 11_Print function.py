6c343dac97d5da9d8bdd0b636d6699a1e13c86f4
Avi102519 <aviswe2016@gmail.com>
2026-05-18 21:38:16 -0500

    Add files via upload

---
+++ 11_Print function.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a= 20
+b=30
+add = a+b
+print(add)
+
+
+# In[4]:
+
+
+a= 20
+b=30
+add = a+b
+print('The addition of', a ,'and' , b , '=' , add)
+
+
+# In[5]:
+
+
+a = 10
+b = 20
+c = 30
+add = a+b+c
+print('The addition of', a, ',' , b ,'and' , c , '=' , add)
+
+
+# In[7]:
+
+
+name = 'Aira'
+age = 6
+city = 'Spring Hill'
+print('My name is', name, ',' 'i am' , age , 'years old' , 'i am from' , city)
+
+
+# # Print with curly braces
+
+# In[8]:
+
+
+print('Myname is {}, i am {} years old and i am from {}' .format(name,age,city))
+
+
+# In[10]:
+
+
+print('The addition of {},{} and {} is {}'.format(a,b,c,add))
+
+
+# # Two Print functions to print on same line
+
+# In[11]:
+
+
+print('Hello', end=' ')
+print('Good Morning')
+
+
+# In[13]:
+
+
+print('Hello', end = '#')
+print('Good Morning')
+
+
+# In[ ]:
+
+
+
+
